b518f1692232bd3efc8532ddf62354636edfbd5e
JuanCaBlandon <84398735+JuanCaBlandon@users.noreply.github.com>
2024-12-16 11:04:08 -0500

    add new models

---
+++ dbt_project/models/silver/tables/aditional_info.sql
@@ -0,0 +1,164 @@
+{{ config(
+		materialized='incremental',
+    unique_key='aditional_info_dw_id',
+    post_hook=[
+        "OPTIMIZE {{ this }} ZORDER BY offense_date ;",
+        "ANALYZE TABLE {{ this }} COMPUTE STATISTICS FOR ALL COLUMNS;"
+        ]
+) }}
+
+with Tmp as(
+SELECT
+  vendor_name,
+  DriversLicenseNumber AS drivers_license_number,
+  FirstName AS first_name,
+  LastName AS last_name,
+  MiddleName AS middle_name,
+  DateOfBirth AS date_of_birth,
+  VIN AS vin,
+  offense_date,
+  repeat_offender,
+  IID_Start_Date as IID_Start_Date,
+  IID_End_Date as IID_End_Date,
+  current_timestamp() as created_at,
+  row_number() over (partition by vendor_name,DriversLicenseNumber ,FirstName,LastName,MiddleName,DateOfBirth,VIN,offense_date,repeat_offender order by offense_date) as num_duplicates
+FROM
+  {{ source('BRONZE', 'aditional_field_customer_ia') }}
+
+
+),
+cleaned_data AS(
+
+SELECT
+  {{ dbt_utils.generate_surrogate_key(['vendor_name','drivers_license_number','first_name','last_name','middle_name','date_of_birth', 'vin', 'offense_date']) }} as aditional_info_dw_id, 
+  'N/A' as customer_dw_id,
+  vendor_name,
+  drivers_license_number,
+  first_name,
+  last_name,
+  middle_name,
+  date_of_birth,
+  vin,
+  offense_date,
+  repeat_offender,
+  IID_Start_Date,
+  IID_End_Date,
+  created_at,
+  1 as is_inconsistent,
+  'duplicates' as type_inconsistent,
+  num_duplicates
+FROM Tmp
+WHERE num_duplicates > 1
+
+UNION ALL
+
+SELECT
+  {{ dbt_utils.generate_surrogate_key(['vendor_name','drivers_license_number','first_name','last_name','middle_name','date_of_birth', 'vin', 'offense_date']) }} as aditional_info_dw_id,
+  'N/A' as customer_dw_id,
+  vendor_name,
+  drivers_license_number,
+  first_name,
+  last_name,
+  middle_name,
+  date_of_birth,
+  vin,
+  offense_date,
+  repeat_offender,
+  IID_Start_Date,
+  IID_End_Date,
+  created_at,
+  1 as is_inconsistent,
+  'NULL values' as type_inconsistent,
+  num_duplicates
+FROM Tmp
+WHERE num_duplicates = 1 AND 
+  (drivers_license_number IS NULL OR first_name IS NULL OR last_name IS NULL  OR date_of_birth IS NULL OR vin IS NULL)
+
+UNION ALL
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['vendor_name','Tmp.drivers_license_number','Tmp.first_name','Tmp.last_name','Tmp.middle_name','Tmp.date_of_birth', 'Tmp.vin', 'Tmp.offense_date']) }} as aditional_info_dw_id,
+    'N/A' as customer_dw_id,
+    vendor_name,
+    Tmp.drivers_license_number,
+    Tmp.first_name,
+    Tmp.last_name,
+    Tmp.middle_name,
+    Tmp.date_of_birth,
+    Tmp.vin,
+    offense_date,
+    repeat_offender,
+    IID_Start_Date,
+    IID_End_Date,
+    Tmp.created_at,
+    1 as is_inconsistent,
+    'Without reference entity' as type_inconsistent,
+    num_duplicates
+FROM Tmp
+LEFT JOIN {{ ref('customer') }} as c
+ON  Tmp.drivers_license_number = c.drivers_license_number AND
+    Tmp.first_name= c.first_name AND
+    Tmp.last_name =c.last_name AND
+    Tmp.date_of_birth = c.date_of_birth AND
+    Tmp.vin = c.vin
+WHERE Tmp.num_duplicates = 1 AND Tmp.drivers_license_number IS NOT NULL AND Tmp.first_name IS NOT NULL 
+  AND Tmp.last_name IS NOT NULL  AND Tmp.date_of_birth IS NOT NULL AND Tmp.vin IS NOT NULL
+  AND c.drivers_license_number IS NULL
+  
+UNION ALL
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['vendor_name','Tmp.drivers_license_number','Tmp.first_name','Tmp.last_name','Tmp.middle_name','Tmp.date_of_birth', 'Tmp.vin', 'Tmp.offense_date']) }} as aditional_info_dw_id,
+    c.customer_dw_id,
+    vendor_name,
+    Tmp.drivers_license_number,
+    Tmp.first_name,
+    Tmp.last_name,
+    Tmp.middle_name,
+    Tmp.date_of_birth,
+    Tmp.vin,
+    offense_date,
+    repeat_offender,
+    IID_Start_Date,
+    IID_End_Date,
+    Tmp.created_at,
+    0 as is_inconsistent,
+    'N/A' as type_inconsistent,
+    num_duplicates
+FROM Tmp
+INNER JOIN {{ ref('customer') }} as c
+ON  Tmp.drivers_license_number = c.drivers_license_number AND
+    Tmp.first_name= c.first_name AND
+    Tmp.last_name =c.last_name AND
+    Tmp.date_of_birth = c.date_of_birth AND
+    Tmp.vin = c.vin
+WHERE Tmp.num_duplicates = 1 AND Tmp.drivers_license_number IS NOT NULL AND Tmp.first_name IS NOT NULL 
+  AND Tmp.last_name IS NOT NULL  AND Tmp.date_of_birth IS NOT NULL AND Tmp.vin IS NOT NULL
+
+)
+
+SELECT
+    aditional_info_dw_id,
+    customer_dw_id,
+    vendor_name,
+    drivers_license_number,
+    first_name,
+    last_name,
+    middle_name,
+    date_of_birth,
+    vin,
+    offense_date,
+    repeat_offender,
+    IID_Start_Date,
+    IID_End_Date,
+    created_at,
+    is_inconsistent,
+    type_inconsistent,
+    num_duplicates
+  FROM cleaned_data
+  {% if is_incremental() %}
+    where aditional_info_dw_id not in (select aditional_info_dw_id from {{ this }})
+{% endif %}
+
+
+
