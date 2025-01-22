695d4318fd8a235a9aa6536c09c029ed1438a19b
Sebastian Osorno <sebastian.osorno@sourcemeridian.com>
2025-01-22 04:03:20 -0500

    Fiz in customer and customer_cleaned logic

--- dbt_project/models/gold/tables/customer.sql
+++ dbt_project/models/gold/tables/customer.sql
@@ -11,7 +11,7 @@ post_hook=[
 
 
 select
-    customer_dw_id,
+    customer_dw_id,,
     customer_reporting_state_id,
     customer_id,
     drivers_license_number,
@@ -22,17 +22,23 @@ select
     vin,
     install_date,
     deinstall_date,
-    device_log_rptg_class_cd,
-    active_status,
     state_code,
+    active_status,
     report_status_cd,
     customer_status,
+    active_status_start_date,
+    active_status_end_date,
     effective_start_date,
     effective_end_date,
-    active_status_start_date,
+    device_log_rptg_class_cd,
+    create_date,
     create_user,
     modify_date,
     modify_user,
+    repeat_offender,
+    offense_date,
+    iid_start_date,
+    iid_end_date,
     created_at
 from {{ ref('customer_cleaned') }}
 where is_inconsistent = 0
