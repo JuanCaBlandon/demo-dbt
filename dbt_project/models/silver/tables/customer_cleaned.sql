1fc1e025ef5cc54f76f9595fa89618a879315208
JuanCaBlandon <84398735+JuanCaBlandon@users.noreply.github.com>
2025-02-15 12:37:11 -0500

    add Max Character Limit inconsistent

--- dbt_project/models/silver/tables/customer_cleaned.sql
+++ dbt_project/models/silver/tables/customer_cleaned.sql
@@ -157,6 +157,50 @@ SELECT
     iid_start_date,
     iid_end_date,
     created_at,
+    1 as is_inconsistent,
+    'Max Character Limit' as type_inconsistent,
+    num_duplicates
+FROM Tmp
+WHERE num_duplicates = 1 AND 
+ (
+    LENGTH(drivers_license_number) > 50 OR 
+    LENGTH(first_name) > 80 OR 
+    LENGTH(last_name) > 80 OR 
+    LENGTH(date_of_birth) > 10 OR 
+    LENGTH(vin) > 30
+  )
+
+UNION ALL
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['customer_id','drivers_license_number', 'first_name','last_name','date_of_birth','vin','effective_start_date','num_duplicates']) }} as customer_dw_id,
+    customer_reporting_state_id,
+    customer_id,
+    drivers_license_number,
+    first_name,
+    last_name,
+    middle_name,
+    date_of_birth,
+    vin,
+    install_date,
+    deinstall_date,
+    state_code,
+    active_status,
+    report_status_cd,
+    customer_status,
+    active_status_start_date,
+    active_status_end_date,
+    effective_start_date,
+    effective_end_date,
+    device_log_rptg_class_cd,
+    create_date,
+    create_user,
+    modify_date,
+    modify_user,
+    repeat_offender,
+    offense_date,
+    iid_start_date,
+    iid_end_date,
+    created_at,
     0 as is_inconsistent,
     'N/A' as type_inconsistent,
     num_duplicates
