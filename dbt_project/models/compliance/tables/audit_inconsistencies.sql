c1118aed8c30d3980fbd0abe22ea49a0e3ce78c8
JuanCaBlandon <84398735+JuanCaBlandon@users.noreply.github.com>
2025-03-10 08:44:47 -0500

    Update audit_inconsistencies.sql

--- dbt_project/models/compliance/tables/audit_inconsistencies.sql
+++ dbt_project/models/compliance/tables/audit_inconsistencies.sql
@@ -1,3 +1,4 @@
+
 {{ config(
     materialized='incremental',
     database='compliance_' ~ var('DEPLOYMENT_ENVIRONMENT'),
@@ -14,7 +15,7 @@ WITH source AS (
         'customer_cleaned' AS source_table_name,
         'ia' as state,
         is_inconsistent,
-        id_inconsistent,
+        inconsistency_id,
         created_at
     FROM state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.silver.customer_cleaned
     WHERE is_inconsistent = 1
@@ -24,34 +25,41 @@ WITH source AS (
         'batch_customer_cleaned' AS source_table_name,
         'ia' as state,
         is_inconsistent,
-        id_inconsistent,
+        inconsistency_id,
         created_at
         
     FROM state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.silver.batch_customer_cleaned
     WHERE is_inconsistent = 1
+
     UNION ALL
     SELECT
         event_dw_id AS source_table_id,
         'customer_events_cleaned' AS source_table_name,
         'ia' as state,
         is_inconsistent,
-        id_inconsistent,
+        inconsistency_id,
         created_at
     FROM state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.silver.customer_events_cleaned
     WHERE is_inconsistent = 1
 
     UNION ALL
-SELECT
-    c.customer_dw_id AS source_table_id,
-    'customer_cleaned' AS source_table_name,
-    'ia' as state,
-    1 AS is_inconsistent,
-    8 AS id_inconsistent,
-    c.created_at
- FROM state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.silver.customer_cleaned as c
+    SELECT
+        c.customer_dw_id AS source_table_id,
+        'customer_cleaned' AS source_table_name,
+        'ia' as state,
+        1 AS is_inconsistent,
+        7 AS inconsistency_id, -- Not present in batch file
+        c.created_at
+    FROM state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.silver.customer_cleaned as c
     LEFT JOIN state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.silver.batch_customer_cleaned as bc 
         ON c.drivers_license_number = bc.drivers_license_number
-        AND RIGHT(bc.vin,6) = RIGHT(c.vin,6)
+        AND (
+            RIGHT(c.vin,6) = RIGHT(bc.vin,6)
+            OR RIGHT(c.vin,5) = RIGHT(bc.vin,5)
+            OR RIGHT(c.vin,4) = RIGHT(bc.vin,4)
+            OR RIGHT(c.vin,3) = RIGHT(bc.vin,3)
+        )
+        AND bc.created_at = "{{ var('execution_date', '2025-01-01') }}"
     WHERE
         c.is_inconsistent = 0
         AND c.is_current = 1
@@ -63,26 +71,38 @@ SELECT
         'batch_customer_cleaned' AS source_table_name,
         'ia' as state,
         1 AS is_inconsistent,
-        10 AS id_inconsistent,
+        9 AS inconsistency_id, -- Not present in actives customer table
         bc.created_at
     FROM state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.silver.batch_customer_cleaned as bc
     LEFT JOIN state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.silver.customer_cleaned as c
-    ON c.drivers_license_number = bc.drivers_license_number
-        AND RIGHT(bc.vin,6) = RIGHT(c.vin,6)
+        ON c.drivers_license_number = bc.drivers_license_number
+        AND (
+            RIGHT(c.vin,6) = RIGHT(bc.vin,6)
+            OR RIGHT(c.vin,5) = RIGHT(bc.vin,5)
+            OR RIGHT(c.vin,4) = RIGHT(bc.vin,4)
+            OR RIGHT(c.vin,3) = RIGHT(bc.vin,3)
+        )
         AND c.is_current = 1
     WHERE
         bc.is_inconsistent = 0
         AND  c.customer_dw_id IS NULL
+        AND bc.created_at = "{{ var('execution_date', '2025-01-01') }}"
+
     UNION ALL
     SELECT
         csr.customer_state_dw_id AS source_table_id,
         'customer_state_reported' AS source_table_name,
-        'ia' as state,
+        'ia' AS state,
         1 AS is_inconsistent,
-        11 AS id_inconsistent,
-        bc.created_at
-    FROM state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.gold.customer_state_reported as csr
-    WHERE csr.status = 0 and action_required = 1
+        CASE
+            WHEN rt.id = 4 THEN 11
+            WHEN rt.id = 5 THEN 12
+            WHEN rt.id = 7 THEN 13
+        END AS inconsistency_id,
+        csr.created_at
+    FROM state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.gold.customer_state_reported csr
+    INNER JOIN state_reporting_{{ var("DEPLOYMENT_ENVIRONMENT") }}.gold.record_type rt USING(record_type_dw_id)
+    WHERE status = 3
 
 ),
 source2 AS (
@@ -91,7 +111,7 @@ source2 AS (
         source_table_id,
         source_table_name,
         is_inconsistent,
-        id_inconsistent,
+        inconsistency_id,
         created_at
     FROM source
 )
@@ -101,7 +121,7 @@ SELECT
     source_table_id,
     source_table_name,
     is_inconsistent,
-    id_inconsistent,
+    inconsistency_id,
     created_at
 FROM source2
 {% if is_incremental() %}
